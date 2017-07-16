fe768848a8629030a323e0df2fdffda0896ccc48
ash0x0 <ahmed.e.elshafey@gmail.com>
2017-07-17 02:53:43 +0300

    Almost full, need to link tab

---
+++ Donor List/BloodTypePickerViewController.swift
@@ -0,0 +1,9 @@
+//
+//  BloodTypePickerViewController.swift
+//  Donor List
+//
+//  Created by Ahmed on 16/07/2017.
+//  Copyright © 2017 Ahmed. All rights reserved.
+//
+
+import Foundation
---
+++ Donor List/RegistrationPickerViewController.swift
@@ -0,0 +1,9 @@
+//
+//  RegistrationPickerViewController.swift
+//  Donor List
+//
+//  Created by Ahmed on 16/07/2017.
+//  Copyright © 2017 Ahmed. All rights reserved.
+//
+
+import Foundation
