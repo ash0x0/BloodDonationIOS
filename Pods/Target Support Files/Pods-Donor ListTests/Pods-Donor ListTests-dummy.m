a26a249a8b15023e0ee7d82648d5b6a020f77600
ash0x0 <ahmed.e.elshafey@gmail.com>
2017-07-18 13:08:55 +0300

    Cleaned repo

--- Pods/Target Support Files/Pods-Donor ListTests/Pods-Donor ListTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Donor_ListTests : NSObject
-@end
-@implementation PodsDummy_Pods_Donor_ListTests
-@end
--- Pods/Target Support Files/Pods-Donor ListUITests/Pods-Donor ListUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Donor_ListUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Donor_ListUITests
-@end
